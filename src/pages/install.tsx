37990b4cc7533382096ed3bf61f1e0a5d5e7666d
thoratica <me@tica.fun>
2023-01-19 23:11:31 +0900

    수동 설치 가이드와 이모티콘 추가 가이드 분리

--- src/pages/install.tsx
+++ src/pages/install.tsx
@@ -40,31 +40,27 @@ const Index: NextPage<{ version: string }> = ({ version }) => {
                   target="_blank"
                   className="text-[22px] font-semibold px-9 py-[18px] leading-4 rounded-lg bg-white/20 backdrop-blur-md text-white text-shadow shadow-md"
                 >
-                  {`Chrome 웹 스토어에서 다운로드`}
+                  {`Chrome 웹 스토어에서 추가하기`}
                 </a>
                 <a
                   href={`https://github.com/thoratica/dutmoticon/archive/refs/tags/${version}.zip`}
                   target="_blank"
                   className="text-[22px] font-semibold px-9 py-[18px] leading-4 rounded-lg bg-white/20 backdrop-blur-md text-white text-shadow shadow-md"
                 >
-                  {`ZIP으로 다운로드 (v${version})`}
+                  {`수동으로 추가하기 (v${version})`}
                 </a>
               </div>
             </div>
           </div>
         </section>
         <section className="max-w-7xl w-full mx-auto px-10 0.5md:px-6 pb-2 mb-8 flex flex-col">
-          <h2 className="text-2xl font-semibold mt-8">설치 가이드</h2>
+          <h2 className="text-[28px] font-semibold mt-8">수동 설치 가이드</h2>
           <div className="mt-2 grid grid-cols-2 md:grid-cols-1 w-full gap-4">
             {[
-              '1. "지금 다운로드" 버튼을 눌러 ZIP 파일을 다운로드하세요.',
+              '1. "수동으로 추가하기" 버튼을 눌러 ZIP 파일을 다운로드하세요.',
               "2. 다운로드한 ZIP 파일의 압축을 해제하세요.",
               "3. 주소창에 chrome://extensions를 입력해 확장 프로그램 페이지로 이동한 뒤 개발자 모드를 활성화하세요.",
               "4. 압축을 해제한 폴더를 확장 프로그램 페이지에 드래그&드롭으로 설치하세요.",
-              "5. 설치가 완료되면 열리는 Dutmoticon 스토어에서 원하는 이모티콘을 클릭하세요.",
-              '6. "Dutmoticon에 추가" 버튼을 클릭해 이모티콘을 추가하세요.',
-              "7. 엔트리 이야기나 작품 댓글에서 추가한 이모티콘을 사용하세요.",
-              "8. 주소창 옆에 있는 확장 프로그램 아이콘을 클릭하면 나오는 팝업에서 이모티콘을 제거할 수 있어요.",
             ].map((step, i) => {
               return (
                 <div
@@ -73,6 +69,32 @@ const Index: NextPage<{ version: string }> = ({ version }) => {
                 >
                   <img
                     src={`/guide/step-${i + 1}.png`}
+                    className="border-b border-b-neutral-100 aspect-video object-cover"
+                  />
+                  <div className="flex flex-col justify-center w-full min-w-0 px-4 py-3">
+                    <span className="text-lg font-medium leading-5">{step}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        <section className="max-w-7xl w-full mx-auto px-10 0.5md:px-6 pb-2 mb-8 flex flex-col">
+          <h2 className="text-[28px] font-semibold mt-8">이모티콘 추가 가이드</h2>
+          <div className="mt-2 grid grid-cols-2 md:grid-cols-1 w-full gap-4">
+            {[
+              "1. 설치가 완료되면 열리는 Dutmoticon 스토어에서 원하는 이모티콘을 클릭하세요.",
+              '2. "Dutmoticon에 추가" 버튼을 클릭해 이모티콘을 추가하세요.',
+              "3. 엔트리 이야기나 작품 댓글에서 추가한 이모티콘을 사용하세요.",
+              "4. 주소창 옆에 있는 확장 프로그램 아이콘을 클릭하면 나오는 팝업에서 이모티콘을 제거할 수 있어요.",
+            ].map((step, i) => {
+              return (
+                <div
+                  className="flex flex-col rounded-xl overflow-hidden border border-neutral-100 shadow-md hover:transform hover:-translate-y-1.5 transition-transform duration-300"
+                  key={i}
+                >
+                  <img
+                    src={`/guide/step-${i + 5}.png`}
                     className="border-b border-b-neutral-100 aspect-video object-cover"
                   />
                   <div className="flex flex-col justify-center w-full min-w-0 px-4 py-3">
